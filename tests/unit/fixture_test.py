0b5b2ebbb9ecfe5ae2262a199bf742bf29cd08d4
aARMANDArthur <armand.arthur@gmail.com>
2022-10-19 15:00:25 +0200

    hotfix email

---
+++ tests/unit/fixture_test.py
@@ -0,0 +1,66 @@
+from unittest import mock
+import pytest
+from ... import server
+from flask import template_rendered
+from contextlib import contextmanager
+import pytest
+
+@pytest.fixture
+def captured_templates(app):
+    recorded = []
+
+    def record(sender, template, context, **extra):
+        recorded.append((template, context))
+
+    template_rendered.connect(record, app)
+    try:
+        yield recorded
+    finally:
+        template_rendered.disconnect(record, app)
+
+@pytest.fixture(scope="session")
+def app():
+    app = server.app
+    app.config.update(({'TESTING': True}))
+    yield app
+
+
+@pytest.fixture
+def client(app):
+    with app.test_client() as client:
+        yield client
+
+@pytest.fixture
+def fixture_emails():
+    return {
+                "email_good" : 
+                {
+                    "email" : "john@simplylift.co"
+                },
+                "email_not_good" : 
+                {
+                    "email" : "john@simplylift"
+                }
+            }
+            
+@pytest.fixture
+def fixture_clubs():
+    return [
+            {
+                "name":"Simply Lift",
+                "email":"john@simplylift.co",
+                "points":"100"
+            },
+            {
+                "name":"Iron Temple",
+                "email": "admin@irontemple.com",
+                "points":"50"
+            },
+            {   "name":"She Lifts",
+                "email": "kate@shelifts.co.uk",
+                "points":"110"
+            }
+        ]
+
+
+
---
+++ tests/unit/login_test.py
@@ -0,0 +1,41 @@
+from ...server import loadCompetitions, loadClubs
+from ...tests.unit.fixture_test import fixture_emails, fixture_clubs, app, client, captured_templates
+import pytest
+from ... import server
+
+
+def test_should_response_200_if_email_good(fixture_clubs, fixture_emails, mocker, client):
+    mocker.patch.object(server, "clubs", fixture_clubs)   
+    response = client.post("/showSummary", data={'email': fixture_emails['email_good']['email']})
+    assert response.status_code == 200
+
+
+def test_should_response_301_if_email_not_good(fixture_clubs, fixture_emails, mocker, client):
+    mocker.patch.object(server, "clubs", fixture_clubs)   
+    response = client.post("/showSummary", data={'email': fixture_emails['email_not_good']['email']})
+    assert response.status_code == 301
+
+# def test_login_logout(fixture_clubs, fixture_emails, mocker, client):
+#     mocker.patch.object(server, "clubs", fixture_clubs)   
+#     response = client.post("/showSummary", data={'email': fixture_emails['email_good']['email']})
+#     response = client.get("/logout")
+#     assert response.status_code == 200
+
+def test_should_have_good_arguments_if_email_good(fixture_clubs, fixture_emails, mocker, client, captured_templates):  
+    mocker.patch.object(server, "clubs", fixture_clubs)   
+    client.post("/showSummary", data={'email': fixture_emails['email_good']['email']})
+    template, context = captured_templates[0]
+
+    # Nom du template
+    assert template.name == 'welcome.html'
+
+    # Vérifie si le club existe
+    assert "club" in context
+
+    # Vérifie si les compétitions existent
+    assert "competitions" in context
+
+    # Vérifie si le club est dans la fixture
+    assert context['club'] in fixture_clubs
+
+
